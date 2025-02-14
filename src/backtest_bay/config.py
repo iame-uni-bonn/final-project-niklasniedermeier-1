bcaa8268a60ab38e9b46868c884ad1bfc5497fd0
Niklas Niedermeier <niedermeier.niklas@web.de>
2025-02-14 10:55:04 +0100

    Create the initial project template.

---
+++ src/backtest_bay/config.py
@@ -0,0 +1,8 @@
+"""All the general configuration of the project."""
+
+from pathlib import Path
+
+SRC = Path(__file__).parent.resolve()
+ROOT = SRC.joinpath("..", "..").resolve()
+
+BLD = ROOT.joinpath("bld").resolve()
